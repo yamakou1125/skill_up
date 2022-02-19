fecc61fe40fc21466e51eb8c1791465d8aae5fe3
yamakou1125 <yamakou.93.11.25@gmail.com>
2022-02-19 10:50:58 +0000

    [Add] itemsコントローラ/newアクションとcreateアクション内を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,18 @@
 class ItemsController < ApplicationController
 
 def new
+    @item = Item.new
 end
 
 def create
+    @item = Item.new(item_params)
+      @item.user_id = current_user.id
+    if @item.save
+      flash[:notice] = "登録が完了しました。"
+      redirect_to new_items
+    else
+      render :new
+    end
 end
 
 end
